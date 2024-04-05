6784dc1e4e6685981a98e8474d949eb0ce8dbb65
ryanwolhuter <dev@ryanwolhuter.com>
2024-04-05 15:17:34 +0200

    feat: update inputs
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- index.ts
+++ index.ts
@@ -1,34 +1,34 @@
 import { computeFixedIncomeAssetDerivedFields } from "./src/calculations";
-import { ASSET_PURCHASE } from "./src/constants";
-import { makeMockGroupTransactions } from "./src/data";
+import { ASSET_PURCHASE, ASSET_SALE } from "./src/constants";
 
-const mockAssetSaleTransactions = makeMockGroupTransactions(10, {
-  type: "AssetSale",
-  entryTime: "2021-10-01 00:00:00",
-  fees: [{ amount: 100 }],
-  cashTransaction: {
-    amount: 1000,
-    entryTime: "2021-10-01",
+const mockAssetSaleTransactions = [
+  {
+    type: ASSET_SALE,
+    entryTime: "2022-01-01",
+    fees: [{ amount: 100 }],
+    cashTransaction: {
+      amount: 1000,
+      entryTime: "2022-01-01",
+    },
+    fixedIncomeTransaction: {
+      amount: 10,
+      entryTime: "2022-01-01",
+    },
   },
-  fixedIncomeTransaction: {
-    amount: 1000,
-    entryTime: "2021-10-01",
+  {
+    type: ASSET_SALE,
+    entryTime: "2022-03-15",
+    fees: [{ amount: 100 }],
+    cashTransaction: {
+      amount: 1000,
+      entryTime: "2022-03-15",
+    },
+    fixedIncomeTransaction: {
+      amount: 5,
+      entryTime: "2022-03-15",
+    },
   },
-});
-
-// const mockAssetPurchaseTransactions = makeMockGroupTransactions(10, {
-//   type: "AssetPurchase",
-//   entryTime: "2021-10-01 00:00:00",
-//   fees: [{ amount: 100 }],
-//   cashTransaction: {
-//     amount: 1000,
-//     entryTime: "2021-10-01",
-//   },
-//   fixedIncomeTransaction: {
-//     amount: 1000,
-//     entryTime: "2021-10-01",
-//   },
-// });
+];
 
 const mockAssetPurchaseTransactions = [
   {
